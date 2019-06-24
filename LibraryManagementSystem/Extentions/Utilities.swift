f5cfbd6e2849a98348c47a8ff2946fa98941ecc3
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-24 06:37:41 -0400

    [Ankita Pabbi] Created a Utilities class under Extensions group , created extensions for email check, country code, postal check and phone number check

---
+++ LibraryManagementSystem/Extentions/Utilities.swift
@@ -0,0 +1,40 @@
+//
+//  Utilities.swift
+//  LibraryManagementSystem
+//
+//  Created by Ankita Pabbi on 2019-06-24.
+//  Copyright © 2019 Ankita Pabbi. All rights reserved.
+//
+
+import Foundation
+extension String{
+    func  isVAlidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
+extension String{
+    func countryCode() -> String{
+        return "+1\(self)"
+    }
+}
+
+extension String{
+    func postalCheck() -> Bool{
+        if self.count <= 6 {
+            print("Length of Postal code must be equal or greater than 6")
+            return false
+        }
+        return true
+    }
+}
+extension String{
+    func phoneNumberCheck() -> Bool{
+        if self.count <= 10 {
+            print("Length of Phone Number must be equal or greater than 10")
+            return false
+        }
+        return true
+    }
+}
